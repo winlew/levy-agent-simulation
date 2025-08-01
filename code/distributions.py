7f3c0106622f7b6e6e9194855c9b0524dfa2a6a2
Lewin Scholz <lewin.scholz@outlook.de>
2025-08-01 15:14:28 +0200

    correct textsize

--- code/distributions.py
+++ code/distributions.py
@@ -5,29 +5,28 @@ import matplotlib.pyplot as plt
 from scipy.stats import norm
 import matplotlib.cm as cm
 from pathlib import Path
+from config import LATEX_TEXTWIDTH
 
 x = np.linspace(1e-15, 0.9999, 1000)
 mu_values = [1.01, 1.7, 3]
 alpha_values = [0.01, 0.3, 1]
 
-plt.figure(figsize=(10, 10))
+plt.figure(figsize=(LATEX_TEXTWIDTH, LATEX_TEXTWIDTH))
 plt.rcParams.update({
-    "font.size": 16,
-    "axes.labelsize": 16,
-    "xtick.labelsize": 14,
-    "ytick.labelsize": 14,
-    "legend.fontsize": 14
+    "text.usetex": True,
+    "font.size": 10,
+    "font.family": "serif"
 })
 
 blues = cm.Blues(np.linspace(0.4, 0.8, len(mu_values)))
 for mu, color in zip(mu_values, blues):
     power = 1 / x**(1 / (mu - 1))
-    plt.plot(x, power, color=color, label=f"Power Law (μ={mu:.2f})")
+    plt.plot(x, power, color=color, label=f"Power Law ($\mu$={mu:.2f})")
 
 reds = cm.Reds(np.linspace(0.4, 0.8, len(alpha_values)))
 for alpha, color in zip(alpha_values, reds):
     exp = -np.log(x) / alpha + 1
-    plt.plot(x, exp, color=color, label=f"Exponential (α={alpha})")
+    plt.plot(x, exp, color=color, label=f"Exponential ($\\alpha$={alpha})")
 
 gauss = norm.ppf(1 - x / 2) + 1
 plt.plot(x, gauss, color='green', label="Gaussian")
@@ -35,7 +34,7 @@ plt.plot(x, gauss, color='green', label="Gaussian")
 plt.ylim(0.5, 9)
 plt.yticks(range(1, 10))
 plt.grid(linewidth=0.3)
-plt.xlabel(r'$u$')
+plt.xlabel('$u$')
 plt.ylabel("Step Length")
 plt.legend()
 plt.tight_layout()
